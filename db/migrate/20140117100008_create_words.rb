3abe6dd8cc04cbe958774c782841faf725668d28
tonilin <tonilin@gmail.com>
2014-01-17 18:05:34 +0800

    add word model

---
+++ db/migrate/20140117100008_create_words.rb
@@ -0,0 +1,12 @@
+class CreateWords < ActiveRecord::Migration
+  def change
+    create_table :words do |t|
+      t.string :word
+      t.string :explanation
+      t.string :status
+      t.timestamps
+    end
+
+    add_index :words, :word, :unique => true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140117095244) do
+ActiveRecord::Schema.define(version: 20140117100008) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -30,5 +30,15 @@ ActiveRecord::Schema.define(version: 20140117095244) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "words", force: true do |t|
+    t.string   "word"
+    t.string   "explanation"
+    t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "words", ["word"], name: "index_words_on_word", unique: true, using: :btree
 
 end
