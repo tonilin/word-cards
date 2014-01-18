10b323cd27f7a73863c91e093f432c8df4b3b152
tonilin <tonilin@gmail.com>
2014-01-18 20:37:11 +0800

    clear words controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,39 +1,4 @@
 class WordsController < ApplicationController
 
-  def index
-    @word = Word.new
-
-    @cards = current_user.cards.recent.all
-  end
-
-  def create
-    @cards = current_user.cards.recent.all
-
-    @word = Word.find_or_initialize_by_word(word_params[:word])
-
-    if !@word.persisted?
-      if @word.save
-        current_user.add_card!(@word, nil)
-        YahooDicCrawler.new(@word).run
-        return redirect_to words_path
-      else
-        return render :index
-      end
-    end
-
-    current_user.add_card!(@word, @word.explanations.first)
-
-    redirect_to words_path
-  end
-
-
-
-  private
-
-  def word_params 
-
-    params.require(:word).permit(:word)
-  end
-
 
 end
