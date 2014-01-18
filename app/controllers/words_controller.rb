3f40be7e1321454f7a0dffdf4455fc59bbe42f1d
tonilin <tonilin@gmail.com>
2014-01-18 09:59:02 +0800

    debug

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -4,5 +4,25 @@ class WordsController < ApplicationController
     @word = Word.new
   end
 
+  def create
+    group = current_user.words.build(word_params)
+
+    if current_user.save
+      redirect_to words_path
+    else
+      render :index
+    end
+
+
+  end
+
+
+
+  private
+
+  def word_params 
+    params.require(:word).permit(:word)
+  end
+
 
 end
