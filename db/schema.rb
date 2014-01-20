c1b0c9bf5a70f628c6852503ffcbe80d1892b893
tonilin <tonilin@gmail.com>
2014-01-20 18:49:03 +0800

    add user name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140120102649) do
+ActiveRecord::Schema.define(version: 20140120104836) do
 
   create_table "authorizations", force: true do |t|
     t.string   "provider"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20140120102649) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
